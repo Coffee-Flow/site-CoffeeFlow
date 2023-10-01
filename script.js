15897c245d3b0cc1cf9319d365229faad4545b34
Beatriz Gabriela <beatriz.brandao@sptech.school>
2023-10-01 14:32:12 -0300

    Configuração inicial arquivos HTML, CSS e JavaScript e criação SideBar, Header e seção 1 do site

---
+++ script.js
@@ -0,0 +1,8 @@
+// Animation SideNav
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
