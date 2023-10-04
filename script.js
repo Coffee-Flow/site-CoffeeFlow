87bd6876107ba7aeb2a0930fea3d288a0d728713
Beatriz Gabriela <beatriz.brandao@sptech.school>
2023-10-03 23:00:06 -0300

    Criação seção 2 e seção 3, Ajustes na SideNav

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
 // Animation SideNav
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+}
+
+// Buttons Section 3 
+
+var container1 = document.getElementById('container1');
+var container2 = document.getElementById('container2');
+var container3 = document.getElementById('container3');
+
+
+function exibirCon1() {
+    if (container1.style.display == "none" || container2.style.display == "flex" || container3.style.display == "flex") {
+        container1.style.display = "flex";
+        container2.style.display = "none";
+        container3.style.display = "none";
+    }
+}
+
+function exibirCon2() {
+    if (container2.style.display == "none" || container1.style.display == "flex" || container3.style.display == "flex") {
+        container2.style.display = "flex";
+        container1.style.display = "none";
+        container3.style.display = "none";
+    } 
+}
+function exibirCon3() {
+    if (container3.style.display == "none" || container1.style.display == "flex" || container2.style.display == "flex") {
+        container3.style.display = "flex";
+        container1.style.display = "none";
+        container2.style.display = "none";
+    }
 }
