f7525dc30be0351b489de75765e576c1aca2801b
Beatriz Gabriela <beatriz.brandao@sptech.school>
2023-10-07 02:07:02 -0300

    criação do footer

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Animation SideNav
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "270px";
 }
 
 function closeNav() {
